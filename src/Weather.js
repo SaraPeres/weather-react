7ed047dbde0409637d9dfee33597cd0b905cb606
SaraPeres <63808903+SaraPeres@users.noreply.github.com>
2020-06-12 12:21:29 +0100

    Used AJAX to get real data and created Weather.css file

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+import "./Weather.css";
 
 export default function Weather() {
+const [ready, setReady] = useState(false)  
+const [weatherInfo, setWeatherInfo] = useState(null)
+
+function showWeather(response) {
+  console.log(response.data)
+  setWeatherInfo({
+    city: response.data.name,
+    temp: response.data.main.temp,
+    description: response.data.weather[0].main,
+    tempMin:response.data.main.temp_min,
+    tempMax:response.data.main.temp_max,
+    icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+  })
+  setReady(true)
+}
+if (ready) {
   return (
     <div>
-      <h1 id="city-heading">Porto</h1>
+      <h1 id="city-heading">{weatherInfo.city}</h1>
       <h6 id="date">Wed, 20 May 2020 13:47</h6>
       <div className="weather-today shadow">
         <h2>
-          <span id="temperature">18°</span>
+  <span id="temperature">{Math.round(weatherInfo.temp)}°</span>
           <a href="#" id="celsius" className="active units">
             C
           </a>
@@ -17,18 +35,26 @@ export default function Weather() {
           </a>
         </h2>
         <h3>
-          <span id="current-weather-description">Clouds</span>
+  <span id="current-weather-description">{weatherInfo.description}</span>
           <img
-            src="http://openweathermap.org/img/wn/10d@2x.png"
+            src={weatherInfo.icon}
             alt="Weather Icon"
             id="current-weather-icon"
           />
         </h3>
         <h4>
-          <span id="current-min-temp">15˚</span>
-          <span id="current-max-temp">20˚</span>
+  <span>{Math.round(weatherInfo.tempMin)}˚/</span>
+  <span>{Math.round(weatherInfo.tempMax)}˚</span>
         </h4>
       </div>
     </div>
-  );
+  );} else {
+    let city = "Porto"
+    const apiKey = "d3565a1a83ca66a70607e27406dc0152";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather)
+return (
+  <div> Loading... </div>
+)  
+  }
 }
